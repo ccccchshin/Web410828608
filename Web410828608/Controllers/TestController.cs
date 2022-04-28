5ea2a3a45cac070c2e2b912b5de79d40ce6d063d
石辰栩 <parurusalt308@gmail.com>
2022-04-28 20:36:34 +0800

    0428HW

---
+++ Web410828608/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410828608.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+        public ActionResult HW2()
+        {
+            return View();
+        }
+        public ActionResult HW3()
+        {
+            return View();
+        }
+    }
+}
